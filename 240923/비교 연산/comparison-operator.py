ca061964d5256f1b5322d46414192a14b550a08d
seokbinma <58669223+seokbinma@users.noreply.github.com>
2024-09-23 18:16:36 +0900

    Title: 비교 연산, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240923/비교 연산/comparison-operator.py
@@ -0,0 +1,33 @@
+arr= input().split()
+a= int(arr[0])
+b= int(arr[1])
+#a가 b보다 같거나 큰가?
+if a >= b:
+    print(1)
+else:
+    print(0)
+#a가 b보다 큰가?
+if a > b:
+    print(1)
+else:
+    print(0)
+#b가 a보다 같거나 큰가?
+if b >= a:
+    print(1)
+else:
+    print(0)
+#b가 a보다 큰가?
+if b>a:
+    print(1)
+else:
+    print(0)
+#a와 b가 같은가?
+if a == b:
+    print(1)
+else:
+    print(0)
+#a와 b가 다른가?
+if a != b:
+    print(1)
+else:
+    print(0)
