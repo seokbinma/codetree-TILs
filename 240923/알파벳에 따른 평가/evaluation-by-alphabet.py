ad86cd9b19d0b1544f44d98777b0909f6cfca964
seokbinma <58669223+seokbinma@users.noreply.github.com>
2024-09-23 17:32:29 +0900

    Title: 알파벳에 따른 평가, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240923/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+al = input()
+if al == 'S':
+    print("Superior")
+elif al == "A":
+    print('Excellent')
+elif al =='B':
+    print('Good')
+elif al == 'C':
+    print("Usually")
+elif al == 'D':
+    print('Effort')
+else:
+    print("Failure")
