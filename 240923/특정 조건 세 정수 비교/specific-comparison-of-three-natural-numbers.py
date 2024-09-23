1882c9e083b8b786a72cb1388dd7c7fac85a68c2
seokbinma <58669223+seokbinma@users.noreply.github.com>
2024-09-23 18:36:25 +0900

    Title: 특정 조건 세 정수 비교, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240923/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,13 @@
+n = input().split()
+a=int(n[0])
+b=int(n[1])
+c=int(n[2])
+
+if a<b and a<c :
+    print(1,end=" ")
+else:
+    print(0,end=" ")
+if a==b and b==c:
+    print(1)
+else:
+    print(0)
