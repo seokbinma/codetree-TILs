f9ba9e55971e68d14e2e36f74b700ebe0decc1b4
seokbinma <58669223+seokbinma@users.noreply.github.com>
2024-09-24 16:34:45 +0900

    Title: 중앙값 구하기, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240924/중앙값 구하기/find-the-median.py
+++ 240924/중앙값 구하기/find-the-median.py
@@ -1,10 +1,9 @@
 num = input().split()
 a,b,c = int(num[0]),int(num[1]),int(num[2])
 
-if (a>b and a<c) or (a<c and a>b):
-    
+if (a>b and a<c) or (a<b and a>c):
     print(a)
-elif (b>a and b<c) or (b<c and b>a):
+elif (b>a and b<c) or (c<b and b<a):
     print(b)
 elif (c>b and c<a) or (a<c and c<b):
     print(c)
