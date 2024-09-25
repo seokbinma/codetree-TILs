4d119a82a28120e44bc7b6c2b919a85d9aadab17
seokbinma <58669223+seokbinma@users.noreply.github.com>
2024-09-25 22:19:30 +0900

    Title: 369 게임, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240925/369 게임/369-game.py
@@ -0,0 +1,13 @@
+n = input()
+num= int(n)
+
+for i in range(1,num+1):
+    sn=str(i)
+    if '3'in sn:
+        print(0,end=" ")
+    elif '6'in sn:
+        print(0,end=" ")
+    elif '9' in sn:
+        print(0,end=" ")
+    else:
+        print(i,end=" ")
