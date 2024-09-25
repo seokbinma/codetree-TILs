0624a3af37760e1356953a9e79e1349351e65bc0
seokbinma <58669223+seokbinma@users.noreply.github.com>
2024-09-25 23:11:04 +0900

    Title: 윤년 횟수 구하기, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240925/윤년 횟수 구하기/number-of-leap-years.py
+++ 240925/윤년 횟수 구하기/number-of-leap-years.py
@@ -5,7 +5,7 @@ for i in range(1,n+1):
     if i % 4==0:
         if i%100==0:
             if i%400 != 0:
-                break
+                pass
             else:
                 cnt+=1
         else:
