8651b3cf4bbdca1d76d34d1c0aa1ee76dbab193f
seokbinma <58669223+seokbinma@users.noreply.github.com>
2024-09-26 15:48:06 +0900

    Title: 출석 부르기 2, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240926/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,15 @@
+while True:
+
+    s= int(input())
+
+    if s>4:
+        print("Vacancy")
+        break
+    if s ==1 :
+        print("John")
+    elif s==2:
+        print("Tom")
+    elif s==3:
+        print("Paul")
+    elif s==4:
+        print("Sam")
