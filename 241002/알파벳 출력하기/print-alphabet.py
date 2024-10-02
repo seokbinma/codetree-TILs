3475d9247a1593e54b4f3aef3e8d76684be63d34
seokbinma <58669223+seokbinma@users.noreply.github.com>
2024-10-02 13:49:56 +0900

    Title: 알파벳 출력하기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241002/알파벳 출력하기/print-alphabet.py
+++ 241002/알파벳 출력하기/print-alphabet.py
@@ -3,7 +3,10 @@ n=int(input())
 
 for i in range(n):
     for j in range(n):
+        if chr(a)=="Z":
+            a=65
         if j<=i:
             print(chr(a),end="")
             a+=1
+        
     print()
