ed848b731afffba121786362cbb0ff6d50cc5791
seokbinma <58669223+seokbinma@users.noreply.github.com>
2024-10-02 10:38:15 +0900

    Title: 구구단 만들기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 241002/구구단 만들기/print-multiplication-table.py
+++ 241002/구구단 만들기/print-multiplication-table.py
@@ -3,7 +3,7 @@ a,b=int(arr[0]),int(arr[1])
 
 
 for i in range(1,10):
-    for j in range(b,a-1,-a):
+    for j in range(b,a-1,-2):
         print(f"{j} * {i} = {j*i}",end=" ")
         if j > a:
             print("/",end=' ')
